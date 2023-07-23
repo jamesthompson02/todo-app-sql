df7084e1fe9a611128b718283960407aaf75e2e1
jamesthompson02 <jamesthompsongithub@outlook.com>
2023-07-23 17:37:42 +0100

    updated server.js file to integrate useraccountrouter into server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const authRouter = require('./routes/auth');
 const dashboardRouter = require('./routes/dashboard');
 const todoRouter = require('./routes/todos');
+const userAccountRouter = require('./routes/userAccountActions');
 
 server.use(cors());
 server.use(express.json());
@@ -12,6 +13,7 @@ server.use(express.json());
 server.use('/auth', authRouter);
 server.use('/dashboard', dashboardRouter);
 server.use('/todo', todoRouter);
+server.use('/account', userAccountRouter);
 
 server.get('/', (req, res) => {
     res.send(`<h1>Server has been successfully served<h1>`);
